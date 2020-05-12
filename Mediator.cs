5bfa677ab93c76cfc7ae8920c3c26a2b6c0c123e
Jagepard <dankorot@gmail.com>
2020-05-12 16:09:51 +0300

    add AbstractListener

--- Mediator.cs
+++ Mediator.cs
@@ -1,4 +1,4 @@
-﻿/**
+﻿/*
  * @author  : Jagepard <jagepard@yandex.ru>
  * @license https://mit-license.org/ MIT
  */
@@ -9,20 +9,20 @@ using System.Reflection;
 
 namespace CsDesignPatterns_Mediator
 {
-    class Mediator : IMediator
+    internal class Mediator : IMediator
     {
-        public IDictionary<string, IListener> Listeners { get; } = new Dictionary<string, IListener>();
-        public IDictionary<string, string> Events { get; } = new Dictionary<string, string>();
+        private IDictionary<string, AbstractListener> Listeners { get; } = new Dictionary<string, AbstractListener>();
+        private IDictionary<string, string> Events { get; } = new Dictionary<string, string>();
 
-        public void AddListener(string name, IListener listeneer, string methodName)
+        public void AddListener(AbstractListener listener, string methodName)
         {
-            if (Listeners.ContainsKey(name) && Events.ContainsKey(name))
+            if (Listeners.ContainsKey(listener.GetType().Name) && Events.ContainsKey(listener.GetType().Name))
             {
                 throw new Exception("Command already exists");
             }
 
-            Listeners.Add(name, listeneer);
-            Events.Add(name, methodName);
+            Listeners.Add(listener.GetType().Name, listener);
+            Events.Add(listener.GetType().Name, methodName);
         }
 
         public void Notify(string name, IHandler handler)
@@ -32,16 +32,16 @@ namespace CsDesignPatterns_Mediator
                 throw new Exception(name + " does not exist in the Dictionary");
             }
 
-            IListener listener = Listeners[name];
-            string methodName = Events[name];
+            var listener = Listeners[name];
+            var methodName = Events[name];
 
             if (handler == null)
             {
-                listener.GetType().GetMethod(methodName).Invoke(obj: listener, parameters: null);
+                listener.GetType().GetMethod(methodName)?.Invoke(obj: listener, parameters: null);
                 return;
             }
 
-            listener.GetType().GetMethod(methodName).Invoke(obj: listener, parameters: new object[] { handler });
+            listener.GetType().GetMethod(methodName)?.Invoke(obj: listener, parameters: new object[] { handler });
         }
     }
 }
