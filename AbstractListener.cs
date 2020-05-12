16f67d58b0043a4e69edf0e4cc5ccfa54081d041
Jagepard <dankorot@gmail.com>
2020-05-12 16:11:47 +0300

    add file

---
+++ AbstractListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CsDesignPatterns_Mediator
+{
+    public abstract class AbstractListener
+    {
+        public void OnEvent(IHandler handler){
+            handler.Message = this.GetType().Name + ": Fine, thanks!";
+            Console.WriteLine("{0}\n", handler.Message);
+        }
+        
+        public void ToGreet(string listenerName, IMediator mediator, IHandler handler)
+        {
+            Console.WriteLine("{0} How are you {1}?", this.GetType().Name, listenerName);
+            mediator.Notify(listenerName, handler);
+        }
+    }
+}
